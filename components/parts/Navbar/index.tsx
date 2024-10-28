7949b09484a5bc532409b01cd9f7cbe37ba66670
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2024-10-29 01:41:55 +0530

    change next/link to a

--- components/parts/Navbar/index.tsx
+++ components/parts/Navbar/index.tsx
@@ -1,32 +1,28 @@
 import Logo from "@components/ui/Logo";
-import Link from "next/link";
 
 const Navbar = () => {
   return (
-    (<div className="z-10 max-h-[54px] w-full mx-auto h-full flex items-center justify-center fixed bg-white dark:bg-black top-0 left0 shadow-sm transition-all duration-300 ease-in-out dark:shadow-[#eeeeee3d] px-4 tablet:px-[32px]">
+    <div className="z-10 max-h-[54px] w-full mx-auto h-full flex items-center justify-center fixed bg-white dark:bg-black top-0 left0 shadow-sm transition-all duration-300 ease-in-out dark:shadow-[#eeeeee3d] px-4 tablet:px-[32px]">
       <div className="flex items-center justify-between max-w-[650px] w-full">
         <nav className="flex items-center justify-between flex-[2] text-gray-700 dark:text-gray-200">
-          <Link
+          <a
             href="/"
-            className="text-sm transition-all duration-300 ease-in-out underline py-1 px-4 rounded-full hover:bg-gray-200 dark:hover:bg-gray-900">
-            
-              /
-            
-          </Link>
-          <Link
+            className="text-sm transition-all duration-300 ease-in-out underline py-1 px-4 rounded-full hover:bg-gray-200 dark:hover:bg-gray-900"
+          >
+            /
+          </a>
+          <a
             href="/about"
-            className="text-sm transition-all duration-300 ease-in-out underline py-1 px-4 rounded-full hover:bg-gray-200 dark:hover:bg-gray-900">
-            
-              /about
-            
-          </Link>
-          <Link
+            className="text-sm transition-all duration-300 ease-in-out underline py-1 px-4 rounded-full hover:bg-gray-200 dark:hover:bg-gray-900"
+          >
+            /about
+          </a>
+          <a
             href="/projects"
-            className="text-sm transition-all duration-300 ease-in-out underline py-1 px-4 rounded-full hover:bg-gray-200 dark:hover:bg-gray-900">
-            
-              /projects
-            
-          </Link>
+            className="text-sm transition-all duration-300 ease-in-out underline py-1 px-4 rounded-full hover:bg-gray-200 dark:hover:bg-gray-900"
+          >
+            /projects
+          </a>
           <a
             href="https://blog.abhinavrajesh.com"
             className="text-sm transition-all duration-300 ease-in-out underline py-1 px-4 rounded-full hover:bg-gray-200 dark:hover:bg-gray-900"
@@ -38,7 +34,7 @@ const Navbar = () => {
           <Logo />
         </div>
       </div>
-    </div>)
+    </div>
   );
 };
 
